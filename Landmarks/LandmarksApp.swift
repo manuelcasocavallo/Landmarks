a2cf79cbb948495147ec9cc6dac763404580cc75
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-04-26 16:44:51 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Manuel Casocavallo on 26/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
