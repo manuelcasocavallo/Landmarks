c6a3dbfd891f82148c11263a00af10c342745f47
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-04-26 19:33:11 +0200

    Final version

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Manuel Casocavallo on 26/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
